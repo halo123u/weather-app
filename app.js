47821e087c7faddcdc59100ff551dd4df6e11301
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-19 21:24:45 -0400

    Filter output to just Adress Latitude and Longitude

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20226E%20122%20st%20NewYork',
     json: true
 },(error,response,body) =>{
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
