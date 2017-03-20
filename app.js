78c6e624a053e5628e13223bb60ff4106246beb2
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-20 16:01:23 -0400

    move logic code into its own file to make the application easier to read

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
     .options({
@@ -14,19 +15,4 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddr = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`,
-    json: true
-}, (error,response,body) => {
-    if(error){
-        console.log('Unable to connect to Google servers. Please check that you are connected to the internet.');
-    } else if(body.status === 'ZERO_RESULTS'){
-        console.log('Invalid address!');
-    } else if(body.status === 'OK'){
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`lat: ${body.results[0].geometry.location.lat}`);
-        console.log(`lng: ${body.results[0].geometry.location.lng}`);
-    }
-});
+geocode.geocodeAddress(argv.address);
