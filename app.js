a45614d1d6563688540a59c5dd7dfa6c227f4d12
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-19 20:55:43 -0400

    update code to show all of the property from the api request

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20226E%20122%20st%20NewYork',
     json: true
 },(error,response,body) =>{
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
