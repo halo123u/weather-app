70335a269c21cc940440f4a404ab089a86c61157
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-20 15:46:07 -0400

    Adds error handling in the appplication

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ var encodedAddr = encodeURIComponent(argv.address);
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`,
     json: true
-},(error,response,body) =>{
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`lng: ${body.results[0].geometry.location.lng}`);
+}, (error,response,body) => {
+    if(error){
+        console.log('Unable to connect to Google servers. Please check that you are connected to the internet.');
+    } else if(body.status === 'ZERO_RESULTS'){
+        console.log('Invalid address!');
+    } else if(body.status === 'OK'){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`lat: ${body.results[0].geometry.location.lat}`);
+        console.log(`lng: ${body.results[0].geometry.location.lng}`);
+    }
 });
