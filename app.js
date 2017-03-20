3c0992389ab8df92509df6c4d4875d237bd9d527
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-19 22:20:38 -0400

    add user input and encoding to make app retrieve user addresses

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand : true,
+            alias: 'address',
+            describe: 'Address to fetch the weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddr = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20226E%20122%20st%20NewYork',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`,
     json: true
 },(error,response,body) =>{
     console.log(`Address: ${body.results[0].formatted_address}`);
